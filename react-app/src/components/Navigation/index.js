69e1076a234b0bf0ade2b4787937059c5b4dfc49
Liqin Li <liqinli.ehs@gmail.com>
2023-09-06 19:48:01 -0700

    working on landing page

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -8,16 +8,30 @@ function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
+		<>
+			<ul id='navigation-container'>
+				<div className='left-nav'>
+					<NavLink to="/products/all">
+						SHOP
+					</NavLink>
+				</div>
+				<div className='logo'>
+					<NavLink exact to="/">
+						Aromatea
+					</NavLink>
+				</div>
+				<div className='right-nav'>
+					<div>SEARCH</div>
+					<div>ACCOUNT</div>
+					<div>CART</div>
+					{/* {isLoaded && (
+						<div>
+							<ProfileButton user={sessionUser} />
+						</div>
+					)} */}
+				</div>
+			</ul>
+		</>
 	);
 }
 
