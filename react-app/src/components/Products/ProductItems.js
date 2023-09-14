9261498d52db3e77b31c1a958b02311cfcdc031a
Liqin Li <liqinli.ehs@gmail.com>
2023-09-13 18:52:53 -0700

    done css for all product

--- react-app/src/components/Products/ProductItems.js
+++ react-app/src/components/Products/ProductItems.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link, useLocation } from "react-router-dom";
 import { useModal } from "../../context/Modal";
 import { DeleteProduct } from "./DeleteProductModal";
+import { useState } from "react";
 
 export const ProductItem = ({product}) => {
     const location = useLocation();
@@ -12,14 +13,25 @@ export const ProductItem = ({product}) => {
         openModal(<DeleteProduct productId={productId} />)
     }
 
+    const [isHovered, setIsHovered] = useState(false);
+
+    // handle image change based on mouse
+    const handleMouseOver = () => {
+        setIsHovered(true);
+    }
+    const handleMouseOut = () => {
+        setIsHovered(false);
+    }
 
     return (
         <div>
             <div title={product.name}>
                 <div className="product-container">
-                    <div className="first-img">
+                    <div className="first-img" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
                         <Link to={`/products/${product.id}`}>
-                            <img src={product.primaryImg} alt={product.name}></img>
+                        <img className={`primary-image ${isHovered ? 'image-fade-out' : ''}`} src={product.primaryImg} alt={product.name}></img>
+
+                        <img className={`secondary-image ${isHovered ? 'image-fade-in' : ''}`} src={product.secondaryImg} alt={product.name}></img>
                         </Link>
                         {location.pathname === '/products/current' && (
                             <div className="product-btns">
@@ -38,6 +50,9 @@ export const ProductItem = ({product}) => {
                         <div className="product-price">
                             ${product.price}
                         </div>
+                    </div>
+                    <div className="product-type">
+                        Organic {product.category} Tea
                     </div>
                 </div>
             </div>
