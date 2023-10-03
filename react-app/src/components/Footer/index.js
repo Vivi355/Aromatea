8f2422567f5f4c8fbbb3de4f89cfcf78dba90287
Liqin Li <liqinli.ehs@gmail.com>
2023-10-03 15:00:13 -0700

    fixed css and footer redirect

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,7 +1,7 @@
 import './Footer.css'
 
 export const Footer = () => {
-    const creator = {name: "Vivian Li", github: "https://github.com/Vivi355", linkedin: "https://www.linkedin.com/in/liqin-li-880646144/"};
+    const creator = {name: "Vivian Li", github: "https://github.com/Vivi355", linkedin: "https://www.linkedin.com/in/vivian-liqin-li/"};
 
     return (
         <>
@@ -10,10 +10,10 @@ export const Footer = () => {
                     <i className="far fa-copyright"></i>2023 Aromatea. All Rights Reserved
                 </div>
                 <div className='contact'>
-                    <a href={creator.github}>
+                    <a href={creator.github} target='_blank'>
                         <i className='fab fa-github'></i>
                     </a>
-                    <a href={creator.linkedin}>
+                    <a href={creator.linkedin} target='_blank'>
                         <i className="fab fa-linkedin"></i>
                     </a>
                 </div>
